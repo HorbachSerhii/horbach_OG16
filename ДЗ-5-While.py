1e86d3dffc5cc1d3f0551a45bb4533c610357d88
HorbachSerhii <83578387+HorbachSerhii@users.noreply.github.com>
2021-05-03 14:02:41 +0300

    Add files via upload

---
+++ ДЗ-5-While.py
@@ -0,0 +1,18 @@
+v1 = float(input("Write some number value (1-10):   "))
+v2 = float(input("Write some number value (11-30):   "))
+v3 = float(input("Write some number value (1-10):   "))
+
+while v1 < v2:
+    if v1 < v2:
+        v4 = v1+v3
+    print(v4, "====",v1,"+",v3)
+    v1 = v1 + v3;
+    if v1 >= v2:
+        print("Hurray!!! YU-HU!!!", v1  )
+
+
+
+
+
+
+
